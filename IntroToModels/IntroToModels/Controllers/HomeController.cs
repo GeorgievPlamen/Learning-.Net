fd7f9ccecbc4efa45e7df363474cce7726997dba
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2023-09-24 13:56:33 +0300

    Models

---
+++ IntroToModels/IntroToModels/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IntroToModels.Models;
+
+namespace IntroToModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Content("At Home page.");
+        }
+
+        [Route("/order")]
+        public IActionResult Order(Order order)
+        {
+            if(!ModelState.IsValid) 
+            {
+                foreach(var model in ModelState.Values)
+                {
+                    foreach(var err in model.Errors)
+                    {
+                        if (err != null)
+                        {
+                            return Content(err.ErrorMessage);
+                        }
+                    }
+                }
+            }
+            order.OrderNo = new Random().Next(1000);
+            return Json(order.OrderNo);
+        }
+    }
+}
---
+++ IntroToModels/IntroToModels/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using IntroToModels.CustomValidators;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace IntroToModels.Models
+{
+    public class Order : IValidatableObject
+    {
+        [BindNever]
+        public int? OrderNo { get; set; }
+        [Required]
+        [MinimumYearValidator]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Please enter valid double value.")]
+        public double InvoicePrice { get; set; }
+        [Required]
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Products.Count < 1)
+            {
+                yield return new ValidationResult("There has to be atleast one product per order");
+            }
+            double _price = 0;
+            foreach(Product product in Products)
+            {
+                _price += product.Price * product.Quantity;
+            }
+            if(InvoicePrice != _price)
+            {
+                yield return new ValidationResult("InvoicePrice doesn't match with the total cost of the specified products in the order.");
+            }
+        }
+    }
+}
