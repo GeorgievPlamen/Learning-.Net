d84a1945198ebfb583ca5981a580e80d06afa5f3
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2023-09-28 11:28:11 +0300

    Learning LayoutViews. Same Weather App.

---
+++ 07.LayoutViews/07.LayoutViews/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using _07.LayoutViews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _07.LayoutViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<CityWeather> cities = new List<CityWeather>()
+        {
+            new CityWeather() {CityUniqueCode = "LDN", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+            new CityWeather() {CityUniqueCode = "NYC", CityName = "New York City", DateAndTime = DateTime.Parse("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+            new CityWeather() {CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = DateTime.Parse("2030-01-01 9:00"),  TemperatureFahrenheit = 82}
+        };
+        [Route("/")]
+        public IActionResult Index()
+        {
+            HttpContext.Response.StatusCode = 200;
+            return View(cities);
+        }
+
+        [Route("/weather/{cityCode}")]
+        public IActionResult CityWeather(string cityCode)
+        {
+            CityWeather? city = cities.Where(temp => temp.CityUniqueCode == cityCode).FirstOrDefault();
+            if (city == null)
+            {
+                HttpContext.Response.StatusCode = 400;
+                return Content("City cannot be null");
+            }
+            HttpContext.Response.StatusCode = 200;
+            return View(city);
+        }
+    }
+}
---
+++ 07.LayoutViews/07.LayoutViews/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _07.LayoutViews.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("search-product")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+        [Route("order-product")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
