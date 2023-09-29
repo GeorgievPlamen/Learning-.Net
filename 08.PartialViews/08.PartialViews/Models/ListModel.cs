4918998b6e0ebd6c0659763bfe4f4aec002cbf28
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2023-09-29 10:38:19 +0300

    Partial Views.

---
+++ 08.PartialViews/08.PartialViews/Models/ListModel.cs
@@ -0,0 +1,8 @@
+﻿namespace _08.PartialViews.Models
+{
+    public class ListModel
+    {
+        public string Title { get; set; } = "";
+        public List<string> Names { get; set; } = new List<string>();
+    }
+}
