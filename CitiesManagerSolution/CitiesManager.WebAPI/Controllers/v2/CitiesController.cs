177af0ed821d68e290998bf0e130df97cac75104
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2023-11-16 09:42:25 +0200

    Added Angular to citiesmanager

--- CitiesManagerSolution/CitiesManager.WebAPI/Controllers/v2/CitiesController.cs
+++ CitiesManagerSolution/CitiesManager.WebAPI/Controllers/v2/CitiesController.cs
@@ -5,36 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CitiesManager.WebAPI.DatabaseContext;
-using CitiesManager.WebAPI.Models;
+using CitiesManager.Infrastructure.DatabaseContext;
+using CitiesManager.Core.Models;
 
 namespace CitiesManager.WebAPI.Controllers.v2
 {
-    [Route("api/v{version:apiVersion}/[controller]")]
-    [ApiController]
-    [ApiVersion("2.0")]
-    public class CitiesController : ControllerBase
-    {
-        private readonly ApplicationDbContext _context;
+ [ApiVersion("2.0")]
+ public class CitiesController : CustomControllerBase
+ {
+  private readonly ApplicationDbContext _context;
 
-        public CitiesController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+  public CitiesController(ApplicationDbContext context)
+  {
+   _context = context;
+  }
 
-        // GET: api/Cities
-        /// <summary>
-        /// Gets all cities from "cities" table, witch ID and Name.
-        /// </summary>
-        /// <returns>All cities</returns>
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<string>>> GetCities()
-        {
-          if (_context.Cities == null)
-          {
-              return NotFound();
-          }
-            return await _context.Cities.Select(temp=> temp.CityName).ToListAsync();
-        }
-    }
+  // GET: api/Cities
+  /// <summary>
+  /// To get list of cities (only city name) from 'cities' table
+  /// </summary>
+  /// <returns></returns>
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<string?>>> GetCities()
+  {
+   var cities = await _context.Cities
+    .OrderBy(temp => temp.CityName)
+    .Select(temp => temp.CityName)
+    .ToListAsync();
+   return cities;
+  }
+ }
 }
